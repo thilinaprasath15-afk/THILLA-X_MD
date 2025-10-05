6f5ba9da5031cbc18388dd90db7bcd125b653a9e
thilinaprasath15-afk <thilinaprasath15@gmail.com>
2025-10-05 12:27:59 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝐈𝐠𝐫𝐢𝐬_𝐗𝐝',
+  author: 'NEXUS',
+  botName: "𝐈𝐠𝐫𝐢𝐬_𝐗𝐝",
+  botOwner: 'NEXUS', // Your name
+  ownerNumber: '2349020090826', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+};
+
+module.exports = settings;
